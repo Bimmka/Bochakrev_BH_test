249144df7b70c691b84593cfc3acce580c527ce9
Bimmka <bibimmka@gmail.com>
2022-08-24 21:58:00 +0400

    Update scene

---
+++ Assets/Features/LevelUtilities/Editor/ObjectSpawnerEditor.cs
@@ -0,0 +1,58 @@
+using Features.LevelUtilities.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace Features.LevelUtilities.Editor
+{
+  [CustomEditor(typeof(ObjectSpawner))]
+  public class ObjectSpawnerEditor : UnityEditor.Editor
+  {
+    private ObjectSpawner myTarget;
+
+    private Transform spawnPosition;
+    private Transform spawnParent;
+    private GameObject spawnObject;
+    private Vector3 spawnDirection;
+    private Vector3 rotation;
+    private float offset;
+    private int count;
+    
+    public override void OnInspectorGUI()
+    {
+      myTarget = (ObjectSpawner) target;
+      
+      spawnPosition = (Transform) serializedObject.FindProperty("startPosition").objectReferenceValue;
+      spawnParent = (Transform) serializedObject.FindProperty("spawnParent").objectReferenceValue;
+      spawnObject = (GameObject) serializedObject.FindProperty("spawnObject").objectReferenceValue;
+      spawnDirection = serializedObject.FindProperty("spawnDirection").vector3Value;
+      rotation = serializedObject.FindProperty("rotation").vector3Value;
+      offset = serializedObject.FindProperty("offset").floatValue;
+      count = serializedObject.FindProperty("count").intValue;
+      
+      base.OnInspectorGUI();
+      
+      if (GUILayout.Button("Spawn"))
+      {
+        Spawn();
+      }
+    }
+
+    private void Spawn()
+    {
+      Vector3 currentSpawnPosition = spawnPosition.position;
+      Vector3 shiftPosition = spawnDirection * offset;
+      GameObject spawnedObject;
+      
+      Undo.IncrementCurrentGroup();
+      for (int i = 0; i < count; i++)
+      {
+        currentSpawnPosition += shiftPosition;
+        spawnedObject = PrefabUtility.InstantiatePrefab(spawnObject) as GameObject;
+        spawnedObject.transform.position = currentSpawnPosition;
+        spawnedObject.transform.rotation = Quaternion.Euler(rotation);
+        spawnedObject.transform.SetParent(spawnParent);
+        Undo.RegisterCreatedObjectUndo(spawnedObject, $"Create Object {spawnedObject.name+i}");
+      }
+    }
+  }
+}
---
+++ Assets/Features/LevelUtilities/Scripts/ObjectSpawner.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Features.LevelUtilities.Scripts
+{
+  public class ObjectSpawner : MonoBehaviour
+  {
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform spawnParent;
+    [SerializeField] private GameObject spawnObject;
+    [SerializeField] private Vector3 spawnDirection;
+    [SerializeField] private Vector3 rotation;
+    [SerializeField] private int count;
+    [SerializeField] private float offset;
+
+    public void Spawn()
+    {
+      
+    }
+  }
+}
